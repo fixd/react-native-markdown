5778b1b98b63d3dac97a433098fadfaa71650c14
Michael Hines <mike@michaelphines.net>
2016-05-31 13:14:27 -0500

    Don't give non-link text onPress so they don't override parent actions

--- rules/text.js
+++ rules/text.js
@@ -12,15 +12,24 @@ export default {
       replace(/ /g, "\u00A0");
     const words = text.split(/\b(?![ \u00A0])/);
     return words.map((word, i) => {
-      return (
-        <Text
-          key={`${state.key}::${i}`}
-          style={state.textStyle}
-          onPress={() => state.link && state.onLinkPress && state.onLinkPress(state.link)}
-        >
-          {word}
-        </Text>
-      );
+      const key = `${state.key}::${i}`;
+      if (state.link && state.onLinkPress) {
+        return (
+          <Text
+            key={key}
+            style={state.textStyle}
+            onPress={state.onLinkPress}
+          >
+            {word}
+          </Text>
+        );
+      } else {
+        return (
+          <Text key={key} style={state.textStyle}>
+            {word}
+          </Text>
+        );
+      }
     });
   },
 };
